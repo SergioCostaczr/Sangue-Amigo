1f5137aff242040ad5e7f9d360e2d384c7c605cb
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-30 23:42:00 -0300

    feat: entity usuario com foreign key de conta

---
+++ src/main/java/com/github/sangueamigo/modules/usuario/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.github.sangueamigo.modules.usuario.entity;
+
+import com.github.sangueamigo.modules.conta.entity.Conta;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column
+    private String telefone;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+}
