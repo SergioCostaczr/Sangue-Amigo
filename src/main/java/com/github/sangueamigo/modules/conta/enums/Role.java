788f431615759699cb59df9a10f9e077e1f6aabe
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-30 23:38:34 -0300

    feat: entity conta implementa userdetails e enum role

---
+++ src/main/java/com/github/sangueamigo/modules/conta/enums/Role.java
@@ -0,0 +1,6 @@
+package com.github.sangueamigo.modules.conta.enums;
+
+public enum Role {
+    USUARIO,
+    HOMOCENTRO
+}
---
+++ src/main/java/com/github/sangueamigo/modules/conta/repository/ContaRepository.java
@@ -0,0 +1,6 @@
+package com.github.sangueamigo.modules.conta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContaRepository extends JpaRepository<ContaRepository,Long> {
+}
