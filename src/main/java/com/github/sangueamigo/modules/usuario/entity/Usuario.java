56bc38f63c3b879f167384c3863ff5cb66d3da7c
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-12 13:08:06 -0300

    feat: adiciona enums tipo sanguineo e sexo na entidade usuario

--- src/main/java/com/github/sangueamigo/modules/usuario/entity/Usuario.java
+++ src/main/java/com/github/sangueamigo/modules/usuario/entity/Usuario.java
@@ -1,6 +1,8 @@
 package com.github.sangueamigo.modules.usuario.entity;
 
 import com.github.sangueamigo.modules.conta.entity.Conta;
+import com.github.sangueamigo.modules.usuario.enums.Sexo;
+import com.github.sangueamigo.modules.usuario.enums.TipoSanguineo;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,8 +23,6 @@ public class Usuario {
     @Column(nullable = false)
     private String nome;
 
-
-
     @Column(nullable = false, unique = true)
     private String cpf;
 
@@ -32,8 +32,15 @@ public class Usuario {
     @Column(nullable = false)
     private LocalDate dataNascimento;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoSanguineo tipoSanguineo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Sexo sexo;
+
     @OneToOne
     @JoinColumn(name = "conta_id")
     private Conta conta;
-
 }
