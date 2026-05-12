660e7c253a23d976324a31973141695d6716243c
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-12 14:50:45 -0300

    feat: horarioDisponivel repository

---
+++ src/main/java/com/github/sangueamigo/modules/horariodisponivel/repository/HorarioDisponivelRepository.java
@@ -0,0 +1,7 @@
+package com.github.sangueamigo.modules.horariodisponivel.repository;
+
+import com.github.sangueamigo.modules.horariodisponivel.entity.HorarioDisponivel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HorarioDisponivelRepository extends JpaRepository<HorarioDisponivel, Long> {
+}
