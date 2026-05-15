28070fadaaac2ed1cbab394d81de108f03301cd0
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-15 17:52:51 -0300

    feat: Docao repo

---
+++ src/main/java/com/github/sangueamigo/modules/doacao/repository/DocaoRepository.java
@@ -0,0 +1,6 @@
+package com.github.sangueamigo.modules.doacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocaoRepository extends JpaRepository<DocaoRepository,Long> {
+}
