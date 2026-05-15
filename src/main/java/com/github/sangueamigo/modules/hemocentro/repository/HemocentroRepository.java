124cba95d5f68392466be0e08b5755ab0c9d9b6e
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-15 18:41:06 -0300

    feat: usuario e hemocentro queries

--- src/main/java/com/github/sangueamigo/modules/hemocentro/repository/HemocentroRepository.java
+++ src/main/java/com/github/sangueamigo/modules/hemocentro/repository/HemocentroRepository.java
@@ -3,5 +3,9 @@ package com.github.sangueamigo.modules.hemocentro.repository;
 import com.github.sangueamigo.modules.hemocentro.entity.Hemocentro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HemocentroRepository extends JpaRepository<Hemocentro,Long> {
+    Optional<Hemocentro> findByContaId(Long contaId);
+
 }
