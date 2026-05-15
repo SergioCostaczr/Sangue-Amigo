09c0e2331240fb2cce419e28f1f7638cd9b7690b
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-15 18:05:45 -0300

    feat: campanha repo

---
+++ src/main/java/com/github/sangueamigo/modules/campanha/repository/CampanhaRepository.java
@@ -0,0 +1,7 @@
+package com.github.sangueamigo.modules.campanha.repository;
+
+import com.github.sangueamigo.modules.campanha.entity.Campanha;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
+}
