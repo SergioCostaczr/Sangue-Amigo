ac4051da73fb28961b00b7cb966d67239b947fa1
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-19 12:20:07 -0300

    auth requests. login e cadastro de usuario/hemocentro.

---
+++ src/main/java/com/github/sangueamigo/modules/conta/dto/request/CadastrarUsuarioRequest.java
@@ -0,0 +1,21 @@
+package com.github.sangueamigo.modules.conta.dto.request;
+
+import com.github.sangueamigo.modules.usuario.enums.Sexo;
+import com.github.sangueamigo.modules.usuario.enums.TipoSanguineo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record CadastrarUsuarioRequest(
+        @NotBlank   String nome,
+        @NotBlank   String cpf,
+        @NotBlank   @Email String email,
+        @NotBlank   String senha,
+        @NotNull    LocalDate dataNascimento,
+        @NotNull    TipoSanguineo tipoSanguineo,
+        @NotNull    Sexo sexo,
+                    String telefone
+) {
+}
