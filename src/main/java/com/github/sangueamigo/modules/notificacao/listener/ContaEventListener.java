19040b9f5e5166b76e7550be642ac00a282c162f
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-20 12:43:05 -0300

    feat: adiciona event listener de cadastro

---
+++ src/main/java/com/github/sangueamigo/modules/notificacao/listener/ContaEventListener.java
@@ -0,0 +1,19 @@
+package com.github.sangueamigo.modules.notificacao.listener;
+
+import com.github.sangueamigo.modules.conta.event.UsuarioCadastradoEvent;
+import com.github.sangueamigo.modules.notificacao.service.NotificacaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class ContaEventListener {
+    private final NotificacaoService notificacaoService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onUsuarioCadastro(UsuarioCadastradoEvent event){
+        notificacaoService.enviarBoasVindas(event.email(),event.nome());
+    }
+}
