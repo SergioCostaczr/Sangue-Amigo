eeecd50430b423265de8d05330802b7838128bcd
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-20 12:59:38 -0300

    feat: adiciona metodo em NotificacaoService o para enviar email com o resetToken

--- src/main/java/com/github/sangueamigo/modules/notificacao/service/NotificacaoService.java
+++ src/main/java/com/github/sangueamigo/modules/notificacao/service/NotificacaoService.java
@@ -24,6 +24,19 @@ public class NotificacaoService {
         );
     }
 
+    public void enviarRecuperacaoSenha(String email, String resetToken) {
+        String link = "https://sangueamigo.com/redefinir-senha?token=" + resetToken;
+        enviar(
+                email,
+                "Recuperação de senha - Sangue Amigo",
+                "Recebemos uma solicitação de recuperação de senha.\n\n" +
+                        "Clique no link abaixo para redefinir sua senha (válido por 15 minutos):\n" + link +
+                        "\n Caso o link não funcione copie o token abaixo e volte ao site para continuar a redefinição de senha:\n" +
+                        resetToken +
+                        "\n\nSe não foi você, ignore este e-mail.\n"
+        );
+    }
+
     private void enviar(String destinatario, String assunto, String corpo){
         SimpleMailMessage menssagem = new SimpleMailMessage();
         menssagem.setFrom(remetente);
